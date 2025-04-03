9da056d026c165ab63c899e3f527ec70a0d90390
hariesramdhani <hydrolizedmaltose@gmail.com>
2025-04-03 16:32:36 +0100

    Initial commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Haries' Notes",
+	url: "https://hariesramdhani.github.io/",
+	language: "en",
+	description: "Some notes about my life, experiences and everything.",
+	author: {
+		name: "Haries Ramdhani",
+		email: "ramdhaniharies@gmail.com",
+		url: "https://hariesramdhani.github.io/"
+	}
+}
